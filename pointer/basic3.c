2100fa914b4dabe3f88fac1470efc593e2b5d5c3
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2021-06-27 13:15:29 +0800

    Update basic3.c
    call by dereference compared to call by value

--- pointer/basic3.c
+++ pointer/basic3.c
@@ -4,15 +4,25 @@ copying the variable as a local variable.*/
 #include <stdio.h>
 
 void increment(int *a); 
+void increment2(int a);
 
 int main(void)
 {
     int a = 10;
     increment(&a);
-    printf("%i ", a);
+    printf("%p\n", &a);
 }
 
-void increment(*a)
+void increment(int *a)
 {
     *a = *a + 1;
 }
+
+void increment2(int a)
+{
+    a = a + 1;
+    printf("%p\n", &a);
+}
+
+/* In this program, the address of the original a and the a declared in the function as local variable are different
+, it proves that using local varaible takes more memory to store new value and variable in stack.*/
